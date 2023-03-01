e53c12c4380738ddffeadb598f20052bc357be28
Shejan Shuza <shejan0@gmail.com>
2023-03-01 12:34:34 -0600

    Working code for now

---
+++ JSONScrubber/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Newtonsoft.Json;
+using SpotifyAPI.Web;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace JSONScrubber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<AddedToCollection> addedList = JsonConvert.DeserializeObject<List<AddedToCollection>>(File.ReadAllText("AddedToCollection.json"));
+            List<StreamingHistory> streamingHistories = new List<StreamingHistory>();
+            foreach(string s in Directory.EnumerateFiles(Directory.GetCurrentDirectory()).Where(f => f.Contains("StreamingHistory")))
+            {
+                List<StreamingHistory> filehist = JsonConvert.DeserializeObject<List<StreamingHistory>>(File.ReadAllText(s));
+                foreach(StreamingHistory hist in filehist)
+                {
+                    hist.parseObject();
+                }
+                streamingHistories.AddRange(filehist);
+            }
+            Console.WriteLine(streamingHistories.Count);
+            streamingHistories.OrderBy(hist => hist.EndTime);
+            Console.WriteLine(streamingHistories.Last().EndTime);
+            streamingHistories=streamingHistories.Where(x => x.EndTime <= DateTime.Parse("2023-02-03T18:05:14.000Z") && x.EndTime > DateTime.Parse("2022-12-12")).ToList();
+            Console.WriteLine(streamingHistories.Count);
+            List<EndSong> endSongs = new List<EndSong>();
+            
+
+            foreach (string s in Directory.EnumerateFiles(Directory.GetCurrentDirectory()).Where(f => f.Contains("endsong")))
+            {
+                List<EndSong> filehist = JsonConvert.DeserializeObject<List<EndSong>>(File.ReadAllText(s));
+                foreach (EndSong hist in filehist)
+                {
+                    hist.parseObject();
+                }
+                endSongs.AddRange(filehist);
+            }
+            endSongs=endSongs.OrderBy(x => x.TSDateTime).ToList();
+            //Console.WriteLine(endSongs.Last().TSDateTime);
+            //Console.WriteLine(DateTime.Parse("2022-12-12"));
+            endSongs = endSongs.Where(x => !string.IsNullOrEmpty(x.TrackUri)).ToList();
+            // onsole.WriteLine(endSongs.Count);
+
+            //endSongs = endSongs.Where(x => x.TSDateTime <= DateTime.Parse("2023-02-03T18:05:14.000Z") /*&&  x.TSDateTime > DateTime.Parse("2022-12-12")*/).ToList();
+            //Console.WriteLine(endSongs.Count);
+            string[] diff = File.ReadAllLines("diff.tsv");
+            SortedDictionary<DateTime, string> sortedUris = new SortedDictionary<DateTime, string>();
+            using(var missing = new StreamWriter("missing.tsv"))
+            {
+                foreach (string s in diff)
+                {
+                    string[] vals = s.Split('\t');
+                    //string uri=vals[0],song = vals[1], artists = vals[2];
+                    List<StreamingHistory> playbacks = streamingHistories.Where(hist => hist.trackName == vals[1] && vals[2].Contains(hist.artistName)).ToList();
+                    if (playbacks.Count != 0)
+                    {
+                        Console.WriteLine(playbacks.Count);
+                        Console.WriteLine(playbacks[0].EndTime);
+                        Console.WriteLine(vals[1]);
+                        Console.WriteLine(vals[2]);
+                        if (!sortedUris.ContainsKey(playbacks[0].EndTime))
+                        {
+                            //    Console.Error.WriteLine("Same key");
+                            sortedUris.Add(playbacks[0].EndTime, vals[0]);
+                        }
+                    }
+                    else
+                    {
+                        missing.WriteLine(string.Join("\t", vals[0], vals[1], vals[2]));
+                    }
+                }
+            }
+            using (var writer = new StreamWriter("found.tsv"))
+            {
+                foreach (KeyValuePair<DateTime, string> pair in sortedUris)
+                {
+                    Console.WriteLine(pair.Key);
+                    Console.WriteLine(pair.Value);
+                    writer.WriteLine(pair.Key + "\t" + diff.Where(x => x.Contains(pair.Value)).First());
+                }
+            }
+                
+
+        }
+    }
+    class AddedToCollection
+    {
+        public string timestamp_utc;
+        public string context_time;
+        public DateTime TimeStamp;
+        public string message_item_uri;
+        public string Uri;
+        public string message_set;
+        public void parseObject()
+        {
+            TimeStamp = DateTime.Parse(timestamp_utc);
+            Uri = message_item_uri.Substring(14);
+        }
+    }
+    class StreamingHistory
+    {
+        public string endTime;
+        public string artistName;
+        public string trackName;
+        public long msPlayed;
+        public DateTime EndTime;
+        public void parseObject()
+        {
+            EndTime = DateTime.Parse(endTime);
+        }
+    }
+    class EndSong
+    {
+        public string ts,
+            username, platform,
+            ms_played,
+            conn_country,
+            ip_addr_decrypted,
+            user_agent_decrypted,
+            master_metadata_track_name,
+            master_metadata_album_artist_name,
+            master_metadata_album_album_name,
+            spotify_track_uri,
+            episode_name,
+            episode_show_name,
+            spotify_episode_uri,
+            reason_start, reason_end,
+            shuffle,
+            skipped,
+            offline, offline_timestamp,
+            icognito_mode;
+       public IPAddress IPAddress;
+        public DateTime TSDateTime, OfflineTimeStamp;
+        public string TrackUri;
+        public void parseObject()
+        {
+            IPAddress = IPAddress.Parse(ip_addr_decrypted);
+            TSDateTime = DateTime.Parse(ts);
+            OfflineTimeStamp = DateTimeOffset.FromUnixTimeMilliseconds(Convert.ToInt64(offline_timestamp)).DateTime;
+            if (string.IsNullOrEmpty(spotify_track_uri))
+            {
+                TrackUri = "";
+            }
+            else
+            {
+                string[] splits = spotify_track_uri.Split(":");
+                TrackUri = splits.Last();
+            }
+            
+
+        }
+    }
+}
--- SpotifyAPITool/Program.cs
+++ SpotifyAPITool/Program.cs
@@ -1,12 +1,270 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using ClientDetails;
 namespace SpotifyAPITool
 {
     class Program
     {
-        static void Main(string[] args)
+        private static EmbedIOAuthServer _server;
+        private static string ClientId = IDSecret.ClientId, ClientSecret = IDSecret.ClientSecret;
+        private static string personalplaylistid = IDSecret.PersonalPlayListID, likedplaylistid = IDSecret.LikedPlayListID;
+        private static async Task RunInstace(SpotifyClient client)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                PrivateUser p = await client.UserProfile.Current();
+                var playlist = await client.Playlists.GetItems(personalplaylistid, new PlaylistGetItemsRequest()
+                {
+                    Offset = 0
+                }); //Shejan Personal Playlist
+                int count = (int)playlist.Total;
+                List<FullTrack> outplaylist = new List<FullTrack>();
+                List<FullTrack> outliked = new List<FullTrack>();
+                Console.WriteLine("Grabbing Personal list");
+                using (var writer = new StreamWriter("personalplaylist.tsv")
+                {
+                    AutoFlush=true
+                })
+                {
+                    for (int n = 0; n < count; n += 100)
+                    {
+                        playlist = await client.Playlists.GetItems(personalplaylistid, new PlaylistGetItemsRequest()
+                        {
+                            Offset = n
+                        });
+                        var tracks = playlist.Items;
+                        //tracks.Offset = n;
+                        foreach (var item in tracks)
+                        {
+                            var track = item.Track;
+                            if (track is FullEpisode)
+                            {
+                                Console.WriteLine("BRUH");
+                            }
+                            else if (track is FullTrack)
+                            {
+                                FullTrack fulltrack = track as FullTrack;
+                                writer.WriteLine(item.AddedAt + "\t" + fulltrack.Id + "\t" + fulltrack.Name + "\t" + string.Join('|', fulltrack.Artists.Select(x => x.Name)));
+                                outplaylist.Add(fulltrack);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Unknown type");
+                            }
+                        }
+
+                    }
+                }
+                Console.WriteLine("Grabbing Liked list");
+                playlist = await client.Playlists.GetItems(likedplaylistid, new PlaylistGetItemsRequest()
+                {
+                    Offset = 0
+                }); //Shejan Personal Playlist
+                count = (int)playlist.Total;
+                using (var writer = new StreamWriter("likedplaylist.tsv")
+                {
+                    AutoFlush = true
+                })
+                {
+                    for (int n = 0; n < count; n += 100)
+                    {
+                        playlist = await client.Playlists.GetItems(likedplaylistid, new PlaylistGetItemsRequest()
+                        {
+                            Offset = n
+                        });
+                        var tracks = playlist.Items;
+                        //tracks.Offset = n;
+                        foreach (var item in tracks)
+                        {
+                            var track = item.Track;
+                            if (track is FullEpisode)
+                            {
+                                Console.WriteLine("BRUH");
+                            }
+                            else if (track is FullTrack)
+                            {
+                                FullTrack fulltrack = track as FullTrack;
+                                writer.WriteLine(item.AddedAt + "\t" + fulltrack.Id + "\t" + fulltrack.Name + "\t" + string.Join('|', fulltrack.Artists.Select(x => x.Name)));
+                                outliked.Add(fulltrack);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Unknown type");
+                            }
+                        }
+
+                    }
+                }
+                Console.WriteLine("Playlist Count: " + outplaylist.Count);
+                Console.WriteLine("Liked Count: " + outliked.Count);
+                List<FullTrack> diff = outliked.Where(x => !outplaylist.Select(y => y.Id).Contains(x.Id)).ToList();
+                Console.WriteLine("Diff Count: " + diff.Count);
+                FullPlaylist newplaylist = await client.Playlists.Create(p.Id, new PlaylistCreateRequest("diff")
+                {
+                    Public = false,
+                });
+                List<string> diffuris = new List<string>();
+                using (var writer = new StreamWriter("diff.tsv")
+                {
+                    AutoFlush = true
+                })
+                {
+                    foreach (var fulltrack in diff)
+                    {
+                        writer.WriteLine(fulltrack.Id + "\t" + fulltrack.Name + "\t" + string.Join('|', fulltrack.Artists.Select(x => x.Name)));
+                        diffuris.Add(fulltrack.Uri);
+                        if (diffuris.Count >= 100)
+                        {
+                            await client.Playlists.AddItems(newplaylist.Id, new PlaylistAddItemsRequest(diffuris));
+                            diffuris = new List<string>();
+                        }
+                    }
+                }
+                if (diffuris.Count != 0)
+                {
+                    await client.Playlists.AddItems(newplaylist.Id, new PlaylistAddItemsRequest(diffuris));
+
+                }
+                Console.WriteLine("Saved diff");
+
+                var saved = await client.Library.GetTracks(new LibraryTracksRequest()
+                {
+                    Limit=50
+                });
+
+                while (saved.Total != 0)
+                {
+                    Console.WriteLine("Deleting");
+                    await client.Library.RemoveTracks(new LibraryRemoveTracksRequest(saved.Items.Select(x => x.Track.Id).ToList()));
+                    saved = await client.Library.GetTracks(new LibraryTracksRequest()
+                    {
+                        Limit = 50
+                    });
+
+                }
+                Console.WriteLine("Saving playlist again");
+                //save from the end.
+                List<string> songids = new List<string>();
+                Console.WriteLine(DateTime.Now);
+                for (int n = outplaylist.Count - 1; n >= 0; n--)
+                //for(int n =0;n<outplaylist.Count;n++)
+                {
+                    if (!outplaylist[n].IsLocal)
+                    {
+
+                        string id = outplaylist[n].Id;
+                        if (string.IsNullOrEmpty(id))
+                        {
+                            Console.Error.WriteLine("NULL FOUND IN OUTPLAYLIST: " + n);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Saving " + id + ": " + outplaylist[n].Name);
+                            songids.Add(id);
+                            await client.Library.SaveTracks(new LibrarySaveTracksRequest(songids));
+                            await Task.Delay(1000);
+                            songids = new List<string>();
+                        }
+                        
+                    }
+                }
+                if (songids.Count != 0)
+                {
+                    await client.Library.SaveTracks(new LibrarySaveTracksRequest(songids));
+                }
+                Console.WriteLine(DateTime.Now);
+            }
+            catch (Exception e)
+            {
+                if(e is APIPagingException)
+                {
+                    APIPagingException s = e as APIPagingException;
+                    Console.Error.WriteLine(s.Message);
+                    Console.Error.WriteLine(s.StackTrace);
+                    Console.Error.WriteLine(s.HelpLink);
+                    Console.Error.WriteLine(s.HResult);
+                    Console.Error.WriteLine(s.Response);
+                    Console.Error.WriteLine(s.RetryAfter);
+                    throw; 
+                }if(e is APITooManyRequestsException)
+                {
+                    APIException s = e as APIException;
+                    Console.Error.WriteLine(s.Message);
+                    Console.Error.WriteLine(s.StackTrace);
+                    Console.Error.WriteLine(s.HelpLink);
+                    Console.Error.WriteLine(s.HResult);
+                    Console.Error.WriteLine(s.Source);
+                    Console.Error.WriteLine(s.TargetSite);
+                    throw; 
+                }if(e is APIException)
+                {
+                    APIException s = e as APIException;
+                    Console.Error.WriteLine(s.Message);
+                    Console.Error.WriteLine(s.StackTrace);
+                    Console.Error.WriteLine(s.HelpLink);
+                    Console.Error.WriteLine(s.HResult);
+                    throw; 
+                }
+                Console.Error.WriteLine(e.Message);
+                Console.Error.WriteLine(e.StackTrace);
+                throw;
+            }
+
+        }
+
+        static async Task Main(string[] args)
+        {
+            _server = new EmbedIOAuthServer(new Uri("http://localhost:42069/callback"), 42069);
+            await _server.Start();
+            _server.AuthorizationCodeReceived += OnAuthorizationCodeReceived;
+            _server.ErrorReceived += OnErrorReceived;
+            var request = new LoginRequest(_server.BaseUri, ClientId, LoginRequest.ResponseType.Code)
+            {
+                Scope = new List<string> { Scopes.AppRemoteControl, Scopes.PlaylistModifyPrivate, Scopes.PlaylistModifyPublic, Scopes.PlaylistReadCollaborative, Scopes.PlaylistReadPrivate, Scopes.Streaming, Scopes.UgcImageUpload, Scopes.UserFollowModify, Scopes.UserFollowRead, Scopes.UserLibraryModify, Scopes.UserLibraryRead, Scopes.UserModifyPlaybackState, Scopes.UserReadCurrentlyPlaying, Scopes.UserReadEmail, Scopes.UserReadPlaybackPosition, Scopes.UserReadPlaybackState, Scopes.UserReadPrivate, Scopes.UserReadRecentlyPlayed, Scopes.UserTopRead,Scopes.Streaming }
+            };
+            
+            BrowserUtil.Open(request.ToUri());
+            while (Console.ReadLine() != "stop")
+            {
+                await Task.Delay(1000);
+            }
+        }
+        private static async Task OnAuthorizationCodeReceived(object sender, AuthorizationCodeResponse response)
+        {
+            await _server.Stop();
+
+            var config = SpotifyClientConfig.CreateDefault();
+            var tokenResponse = await new OAuthClient(config).RequestToken(
+              new AuthorizationCodeTokenRequest(
+                ClientId, ClientSecret, response.Code, new Uri("http://localhost:42069/callback")
+              )
+            );
+
+            var spotify = new SpotifyClient(tokenResponse.AccessToken);
+            try
+            {
+                await RunInstace(spotify);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
+        }
+
+        private static async Task OnErrorReceived(object sender, string error, string state)
+        {
+            Console.WriteLine($"Aborting authorization, error received: {error}");
+            await _server.Stop();
         }
     }
 }
+
+
